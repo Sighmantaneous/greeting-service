ef27eda6ef9a79b3676515da744731edf4eeab82
sighman <g00286398@atu.ie>
2025-12-03 11:07:53 +0000

    Adding Controller

---
+++ src/main/java/ie/atu/greetingservice/controller/GreetingController.java
@@ -0,0 +1,23 @@
+package ie.atu.greetingservice.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+
+
+
+    @GetMapping("/api/greet/{name}")
+    public String greet(@PathVariable String name) {
+        return "Hello " + name + "from service A!!";
+    }
+
+}
